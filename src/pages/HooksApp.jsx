8901a5ce47b85f93a3ab820ef10da8102c8533dc
Johan Echeverry <jmecheverry90@gmail.com>
2024-11-20 05:47:41 -0500

    Hicimos este repositorio para guardar 3 hooks personalizados sobre Contadores, Formularios y Fetch

---
+++ src/pages/HooksApp.jsx
@@ -0,0 +1,10 @@
+
+export const HooksApp = () => {
+  return (
+    <>
+        <h1>Aplicacion de Hooks</h1>
+        <button type="button" className="btn btn-primary">Primary</button>
+
+    </>
+  )
+}
